94f053296dc2e7af051fbf94b93e21a4bc1eef38
sreeadde <sreeadde@cisco.com>
2025-04-28 12:28:11 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_subnet" "public_subnet" {
 
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.2.0/24"
+  cidr_block        = "10.0.2.0/24-----------------------ll"
   availability_zone = "us-west-2a"
 
   tags = {

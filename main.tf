2a456a7a5831bbe07f59f821eaabd12cf9c74201
sreeadde <sreeadde@cisco.com>
2025-04-28 12:11:23 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main_vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = "0.0.0.0/0^^^^^^^^^^^^^^^^^^^^^^"
     gateway_id = aws_internet_gateway.igw.id
   }
 
@@ -58,7 +58,7 @@ resource "aws_route_table" "public_rt" {
 
 # Associate the public subnet with the route table
 resource "aws_route_table_association" "public_rt_assoc" {
-  subnet_id      = aws_subnet.public_subnet.id
+  subnet_id      = aws_subnet.public_subnet.id&&&&&&&&&&&
   route_table_id = aws_route_table.public_rt.id
 }
 

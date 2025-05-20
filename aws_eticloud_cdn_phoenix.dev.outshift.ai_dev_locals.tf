b267b1b4fd4b405873f6a150a9d88300b7766cc8
sreeadde <sreeadde@cisco.com>
2025-05-20 13:47:56 -0400

    merged_content

---
+++ aws_eticloud_cdn_phoenix.dev.outshift.ai_dev_locals.tf
@@ -0,0 +1,6 @@
+locals {
+  aws_account         = "eticloud"
+  cdn_domain_name     = "phoenix.dev.outshift.ai"
+  route53_account     = "eticloud"
+  bucket_domain_name  = "outshift-phoenix-ui.s3.us-east-2.amazonaws.com"
+}
